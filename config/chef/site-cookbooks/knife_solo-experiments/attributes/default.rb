009da87fd95dee1fce61c2b1ea2afa3b122aff57
Anshul Mengi <anshulmengi@gmail.com>
2015-12-16 16:30:19 +0530

    Postgres recipe plus changes in the database.yml.erb

--- config/chef/site-cookbooks/knife_solo-experiments/attributes/default.rb
+++ config/chef/site-cookbooks/knife_solo-experiments/attributes/default.rb
@@ -6,3 +6,8 @@ default['group'] = 'chef'
 
 default['user']['name'] = 'temp_user'
 default['user']['password'] = '$1$kuEH48lm$hawpU/mY1WWvugdrvReCt1'
+
+
+default['db']['user']['name'] = 'dummy_user'
+default['db']['user']['password'] = 'dummy_password'
+default['db']['root_password'] = 'p@ssw0rd'
