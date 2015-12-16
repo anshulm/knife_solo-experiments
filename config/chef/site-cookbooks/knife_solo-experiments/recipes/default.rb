69f50b7dd2440fcf003d144a81d98074e41f53b8
Anshul Mengi <anshulmengi@gmail.com>
2015-12-16 12:33:18 +0530

    Test default recipe

--- config/chef/site-cookbooks/knife_solo-experiments/recipes/default.rb
+++ config/chef/site-cookbooks/knife_solo-experiments/recipes/default.rb
@@ -1,8 +1,2 @@
-#
-# Cookbook Name:: knife_solo-experiments
-# Recipe:: default
-#
-# Copyright 2015, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+
+execute 'apt-get update'
